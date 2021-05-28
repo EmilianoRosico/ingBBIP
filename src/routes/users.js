bc9bb1ef6a53de94eb82f4a6c4d11e39241cef2d
EmilianoRosico <emiliano19@gmail.com>
2021-05-28 14:35:46 -0300

    Integración LDAP y portal de inicio usuario

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
+var usersController = require('../controllers/usersController')
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', usersController.showLogin);
+router.post('/', usersController.postLogin);
 
 module.exports = router;
