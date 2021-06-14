01bb342c352c442503f3b73f55d0bb3abc8ba011
EmilianoRosico <emiliano19@gmail.com>
2021-06-14 18:25:36 -0300

    Se agrega un filtro en el buscador para impedir la busqueda vacia

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,50 +13,54 @@ router.get('/index', function(req, res, next) {
 
 //BUSCADOR
 router.get('/search', async(req, res) => {
-    try {
-        const devices = await db.devices.findAll({
-            where: {
-                name: {
-                    [Op.like]: `%${req.query.search}%`
-                }
-            },
-            include: [
-                { association: "nodes" },
-                { association: "devicemodels" }
-            ],
-            order: [
-                ['name', 'ASC']
-            ]
-        })
-        const nodes = await db.nodes.findAll({
-            where: {
-                name: {
-                    [Op.like]: `%${req.query.search}%`
-                }
-            },
-            order: [
-                ['name', 'ASC']
-            ]
-        })
-        const versions = await db.versions.findAll({
-            where: {
-                name: {
-                    [Op.like]: `%${req.query.search}%`
-                }
-            },
-            order: [
-                ['name', 'ASC']
-            ],
-            include: [
-                { association: "versiontorole" },
-                { association: "versiondevicemodels" }
-            ]
-        })
-        console.log(devices);
-        res.render('../views/searchResult', { title: `Resultados de la busqueda ${req.query.search}`, devices: devices, nodes: nodes, versions: versions });
-    } catch (error) {
-        console.log(error)
+    if (req.query.search != "") {
+        try {
+            const devices = await db.devices.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${req.query.search}%`
+                    }
+                },
+                include: [
+                    { association: "nodes" },
+                    { association: "devicemodels" }
+                ],
+                order: [
+                    ['name', 'ASC']
+                ]
+            })
+            const nodes = await db.nodes.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${req.query.search}%`
+                    }
+                },
+                order: [
+                    ['name', 'ASC']
+                ]
+            })
+            const versions = await db.versions.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${req.query.search}%`
+                    }
+                },
+                order: [
+                    ['name', 'ASC']
+                ],
+                include: [
+                    { association: "versiontorole" },
+                    { association: "versiondevicemodels" }
+                ]
+            })
+            res.render('../views/searchResult', { title: `Resultados de la busqueda ${req.query.search}`, devices: devices, nodes: nodes, versions: versions });
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        res.render('../views/searchResult', { title: `Debe especificar el parametro que desea buscar...`, devices: [], nodes: [], versions: [] });
     }
+
 })
 
 module.exports = router;
