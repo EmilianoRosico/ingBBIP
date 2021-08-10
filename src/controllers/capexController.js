3b0f175aa046d2ee076fe13b1c1d5c06fc0bebd4
EmilianoRosico <emiliano19@gmail.com>
2021-08-10 16:48:56 -0300

    Se genero estructura /views/capex/* y se agrego detalle de solicitud

--- src/controllers/capexController.js
+++ src/controllers/capexController.js
@@ -2,30 +2,39 @@ const db = require('../database/models');
 
 
 module.exports = {
-    view: async (req, res) => {
+    view: async(req, res) => {
         const solicitud = await db.capexs.findAll({ where: { solicitante: res.locals.user } });
-        res.render('../views/capex', { title: 'Solicitud CAPEX', solicitud: solicitud });
+        res.render('../views/capex/capex', { title: 'Solicitud CAPEX', solicitud: solicitud });
+    },
+    detail: async(req, res) => {
+        const solicitud = await db.capexs.findByPk(req.params.id, { where: { solicitante: res.locals.user } })
+        if (solicitud != null) {
+            res.render('../views/capex/capexDetail', { title: `Solicitud ${req.params.id}`, solicitud: solicitud });
+        } else {
+            res.render('somethingWrong', { title: 'SomethingWrong', error: 'Solicitud no generada por su usuario.' })
+        }
+
     },
     addCapex: (req, res) => {
-        res.render('../views/addCapex', { title: 'Nueva solicitud CAPEX' });
+        res.render('../views/capex/addCapex', { title: 'Nueva solicitud CAPEX' });
     },
     postCapex: async(req, res) => {
         try {
-                await db.capexs.create({
-                    solicitante: res.locals.user,
-                    tituloProyecto: req.body.tituloProyecto,
-                    areaSolicitante: req.body.areaSolicitante,
-                    requerimiento: req.body.requerimiento,
-                    cellId: req.body.cellId.toUpperCase(),
-                    fechaObjetivo: req.body.fechaObjetivo,
-                    redundancia: req.body.redundancia,
-                    bandwidth: req.body.bandwidth,
-                    puertosElectricos: req.body.puertosElectricos,
-                    puertosOpticos: req.body.puertosOpticos,
-                    capacidadPuerto: req.body.capacidadPuerto,
-                    comentarios: req.body.comentarios,
-                })
-                res.redirect('/capex');
+            await db.capexs.create({
+                solicitante: res.locals.user,
+                tituloProyecto: req.body.tituloProyecto,
+                areaSolicitante: req.body.areaSolicitante,
+                requerimiento: req.body.requerimiento,
+                cellId: req.body.cellId.toUpperCase(),
+                fechaObjetivo: req.body.fechaObjetivo,
+                redundancia: req.body.redundancia,
+                bandwidth: req.body.bandwidth,
+                puertosElectricos: req.body.puertosElectricos,
+                puertosOpticos: req.body.puertosOpticos,
+                capacidadPuerto: req.body.capacidadPuerto,
+                comentarios: req.body.comentarios,
+            })
+            res.redirect('/capex/capex');
 
         } catch {
             (error => console.log(error))
--- src/routes/capex.js
+++ src/routes/capex.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', capexController.view);
 router.get('/addCapex', capexController.addCapex);
 router.post('/addCapex', capexController.postCapex);
-
+router.get('/:id', capexController.detail)
 
 
 
