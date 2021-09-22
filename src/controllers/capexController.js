e9aeb649e182a6a87ae75c26bf43c4e8e7832d06
EmilianoRosico <emiliano19@gmail.com>
2021-09-21 22:34:49 -0300

    Se agrega la posibilidad que el usuario edite una solicitud ya cargada

--- src/controllers/capexController.js
+++ src/controllers/capexController.js
@@ -2,11 +2,11 @@ const db = require('../database/models');
 
 
 module.exports = {
-    view: async(req, res) => {
+    view: async (req, res) => {
         const solicitud = await db.capexs.findAll({ where: { solicitante: res.locals.user } });
         res.render('../views/capex/capex', { title: 'Solicitud CAPEX', solicitud: solicitud });
     },
-    detail: async(req, res) => {
+    detail: async (req, res) => {
         const solicitud = await db.capexs.findByPk(req.params.id, { where: { solicitante: res.locals.user } })
         if (solicitud != null) {
             res.render('../views/capex/capexDetail', { title: `Solicitud ${req.params.id}`, solicitud: solicitud });
@@ -18,7 +18,7 @@ module.exports = {
     addCapex: (req, res) => {
         res.render('../views/capex/addCapex', { title: 'Nueva solicitud CAPEX' });
     },
-    postCapex: async(req, res) => {
+    postCapex: async (req, res) => {
         try {
             await db.capexs.create({
                 solicitante: res.locals.user,
@@ -36,6 +36,49 @@ module.exports = {
             })
             res.redirect('/capex');
 
+        } catch {
+            (error => console.log(error))
+        }
+    },
+    editGet: async (req, res) => {
+        try {
+            const solicitud = await db.capexs.findByPk(req.params.id, { where: { solicitante: res.locals.user } })
+            if (solicitud != null) {
+                res.render('../views/capex/editCapex', { title: `Solicitud ${req.params.id}`, solicitud: solicitud });
+            } else {
+                res.render('somethingWrong', { title: 'SomethingWrong', error: 'Solicitud no generada por su usuario.' })
+            }
+        } catch {
+            (error => console.log(error))
+        }
+    },
+    editPost: async (req, res) => {
+        try {
+            const solicitud = await db.capexs.findByPk(req.params.id)
+            if (solicitud != null) {
+                await db.capexs.update({
+                    solicitante: res.locals.user,
+                    tituloProyecto: req.body.tituloProyecto,
+                    areaSolicitante: req.body.areaSolicitante,
+                    requerimiento: req.body.requerimiento,
+                    cellId: req.body.cellId.toUpperCase(),
+                    fechaObjetivo: req.body.fechaObjetivo,
+                    redundancia: req.body.redundancia,
+                    bandwidth: req.body.bandwidth,
+                    puertosElectricos: req.body.puertosElectricos,
+                    puertosOpticos: req.body.puertosOpticos,
+                    capacidadPuerto: req.body.capacidadPuerto,
+                    comentarios: req.body.comentarios,
+                }, {
+                    where: { id: req.params.id }
+                })
+                res.redirect('/capex');
+            } else {
+                res.render('somethingWrong', { title: 'SomethingWrong', error: 'La solicitud que desea editar NO existe!' })
+                console.log("************************************************")
+                console.log("La solicitud " + req.params.id + " NO existe!")
+                console.log("************************************************")
+            }
         } catch {
             (error => console.log(error))
         }
--- src/routes/capex.js
+++ src/routes/capex.js
@@ -5,7 +5,10 @@ var router = express.Router();
 router.get('/', capexController.view);
 router.get('/addCapex', capexController.addCapex);
 router.post('/addCapex', capexController.postCapex);
-router.get('/:id', capexController.detail)
+router.get('/editCapex/:id', capexController.editGet);
+router.post('/edit/:id', capexController.editPost);
+router.get('/:id', capexController.detail);
+
 
 
 
